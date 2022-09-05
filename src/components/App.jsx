4e2969f1b04382f66fe11482e01ce193132cd372
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-09-05 12:23:16 +0300

    done task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import user from '../../src/user.json';
+import { Profile } from './Profile';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
