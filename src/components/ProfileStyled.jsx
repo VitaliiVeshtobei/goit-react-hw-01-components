8703b86d25c6131cf54247a064cdd7fa349748f1
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-09-06 15:56:00 +0300

    comm

---
+++ src/components/ProfileStyled.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const TextName = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 1.19;
+  text-align: center;
+  text-transform: capitalize;
+  margin: 0;
+  margin-top: 10px;
+`;
+
+export const Text = styled.p`
+  margin: 0;
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.19;
+  margin-top: 10px;
+`;
+
+export const SpanText = styled.span`
+  display: block;
+  margin-bottom: 10px;
+`;
